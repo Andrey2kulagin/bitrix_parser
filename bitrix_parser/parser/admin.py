bd49a508a0b63a99f759f597dd5add0de13cd662
Кулагин Андрей Андреевич <s121917@guu.ru>
2023-04-10 15:18:19 +0300

    start edit admin_view

--- bitrix_parser/parser/admin.py
+++ bitrix_parser/parser/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username', 'email', 'first_name', 'last_name', "subscription", "activation_date", "subscription_days",
+        "end_of_subscription")
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'password', 'email', 'first_name', 'last_name', "subscription", "activation_date",
+                "subscription_days",
+                "end_of_subscription")}
+         ),
+    )
+
+
+admin.site.register(User, UserAdmin)
--- bitrix_parser/parser/models.py
+++ bitrix_parser/parser/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from datetime import datetime, timedelta
+
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    choices = (('Активна', 'Активна'),
+               ('Не активна', 'Не активна'),)
+    subscription = models.CharField(verbose_name="Статус подписки", max_length=50, choices=choices,
+                                    default="Не активна", null=True)
+    activation_date = models.DateField(verbose_name="Дата активации подписки", null=True)
+    subscription_days = models.PositiveIntegerField(verbose_name="Длина подписки(дней)", null=True)
+    end_of_subscription = models.DateField(verbose_name="Дата истечения подписки", null=True, blank=True)
+
+
